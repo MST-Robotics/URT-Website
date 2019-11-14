26c37cd1ade0b731c558158c1863dbcd72acfc41
Jack Kufa <jhkmw3@mst.edu>
2019-11-14 17:43:54 -0600

    Setup Internal file structure

---
+++ URT-Website.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace URT_Website.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ URT-Website.Services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace URT_Website.Services
+{
+    public class Class1
+    {
+    }
+}
